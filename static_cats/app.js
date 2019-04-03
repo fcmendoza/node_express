882216cc71802ed5d33eb967349839685192d668
Fernando <fcmendoza@gmail.com>
2019-04-02 21:07:06 -0600

    Added route to insert cats to the database.

--- static_cats/app.js
+++ static_cats/app.js
@@ -37,6 +37,15 @@ app.get('/cats', function(req, res){
 	});
 });
 
+app.get('/cats-insert', function(req, res){
+  connection.query('insert into cats (cat_name) values (?)', [req.query.cat_name], function (error, results, fields) {
+    if (error) res.send(error)
+    else res.json({
+      message: "success"
+    });
+  });
+});
+
 app.listen(3001, function(){
 	console.log('listening on 3001');
 });
