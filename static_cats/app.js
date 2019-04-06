b8c0d5371f65815a06c37a153ecef910dcae6f26
Fernando <fcmendoza@gmail.com>
2019-04-06 13:00:18 -0600

    Renamed POST route. Fixed one form not working after adding a second one.

--- static_cats/app.js
+++ static_cats/app.js
@@ -1,20 +1,3 @@
-/*
-make a database called cats_db
-
-make a table called cats
-    the table should have an id and a place_name column
-
-insert 4 records into the cats table
-
-make an express app with a public folder that is static with an index.html file
-
-make a route /cats
-
-    that sends all the cats from the cats table as json
-
-in the index.html file make an ajax call to the /cats route and make paragraph tags for each cat and put them in the body of the index.html file
-*/
-
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
@@ -50,12 +33,13 @@ app.get('/cats-insert', function(req, res){
   });
 });
 
-app.post('/cats-insert-ajax', function(req, res){
+app.post('/cat', function(req, res){
   connection.query('insert into cats (cat_name) values (?)', [req.body.cat_name], function (error, results, fields) {
     if (error) res.send(error)
-    else res.json({
-      message: "success"
-    });
+    // else res.json({
+    //   message: "success"
+    // });
+    else res.redirect('/');
   });
 });
 
